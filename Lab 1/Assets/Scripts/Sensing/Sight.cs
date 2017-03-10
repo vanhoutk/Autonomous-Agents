db07a0ace3b44ba7ef1f132f516085e71897d208
Kris <vanhoutk@tcd.ie>
2017-03-10 01:56:12 +0000

    Some setup for sensing

---
+++ Lab 1/Assets/Scripts/Sensing/Sight.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class Sight : Sense
+{
+    //RaycastHit2D hit = Physics2D.RaycastAll(Vector2 agentLocation, Vector2 destinationLocation, float distanceAgentCanSee);
+}
